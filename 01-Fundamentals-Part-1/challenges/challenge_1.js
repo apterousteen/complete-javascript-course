3e7788821ea86232d8044bacc6a5b3ea1511fe13
apterousteen <ayzhan312@live.ru>
2022-10-01 22:19:31 +0500

    JS_Fundamentals_1_Complete

---
+++ 01-Fundamentals-Part-1/challenges/challenge_1.js
@@ -0,0 +1,43 @@
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+ */
+
+/*
+let massMark1 = 78;
+const heightMark1 = 1.69;
+let massJohn1 = 92;
+const heightJohn1 = 1.95;
+
+let massMark2 = 95;
+const heightMark2 = 1.88;
+let massJohn2 = 85;
+const heightJohn2 = 1.76;
+
+const bmi = (mass, height) => {
+    return mass / height ** 2;
+}
+
+let markHigherBMI1 = bmi(massMark1, heightMark1) > bmi(massJohn1, heightJohn1);
+let markHigherBMI2 = bmi(massMark2, heightMark2) > bmi(massJohn2, heightJohn2);
+
+console.log(`Data 1: Mark's BMI: ${bmi(massMark1, heightMark1)},
+        John's BMI: ${bmi(massJohn1, heightJohn1)},
+        ${markHigherBMI1}`);
+console.log(`Data 2: Mark's BMI: ${bmi(massMark2, heightMark2)},
+        John's BMI: ${bmi(massJohn2, heightJohn2)},
+        ${markHigherBMI2}`);
+*/
---
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -0,0 +1,4 @@
+let age = 18;
+
+console.log(`you are ${age >= 18 ? 'allowed' : 'not allowed'} to drink`)
+
