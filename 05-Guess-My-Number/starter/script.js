ea10a21c0522b56f11de838a5179392c4eab16ad
apterousteen <ayzhan312@live.ru>
2022-10-09 22:58:05 +0500

    - minor style editing - button event listener

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// Input field and 'check' button
+const numInput = document.querySelector('.guess');
+const checkButton = document.querySelector('.check');
+
+// When the Enter is pressed
+numInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        checkButton.click();
+    }
+});
+
+checkButton.onclick = () => {
+    console.log('button clicked');
+}
+
