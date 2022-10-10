3a2db56c9d12f71b71cc51ada6d6844146b00f2b
apterousteen <ayzhan312@live.ru>
2022-10-10 18:38:55 +0500

    Core logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,8 +1,11 @@
 'use strict';
 
-// Input field and 'check' button
 const numInput = document.querySelector('.guess');
 const checkButton = document.querySelector('.check');
+const secretNumberHTML = document.querySelector('.number');
+const scoreHTML = document.querySelector('.score');
+const highScoreHTML = document.querySelector('.highscore');
+const messageHTML = document.querySelector('.message');
 
 // When the Enter is pressed
 numInput.addEventListener('keypress', (event) => {
@@ -12,7 +15,64 @@ numInput.addEventListener('keypress', (event) => {
     }
 });
 
-checkButton.onclick = () => {
-    console.log('button clicked');
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let highScore = 0;
+let score = 20;
+
+console.log(secretNumber)
+
+const checkGuessedNumber = () => {
+    let guessedNumber = numInput.value;
+    // no input
+    if (!guessedNumber) {
+        messageHTML.textContent = `Number? (-_-)`;
+    } else if (score > 1) {
+        // range check
+        if (+guessedNumber < 1 || +guessedNumber > 20) {
+            score--;
+            scoreHTML.textContent = String(score);
+            messageHTML.textContent = `Between 1 and 20 (-_-)`;
+
+        }
+        // winning
+        else if (+guessedNumber === secretNumber) {
+            secretNumberHTML.textContent = String(secretNumber);
+            highScore = score;
+            highScoreHTML.textContent = String(highScore);
+            messageHTML.textContent = `Correct! (^_^)`;
+            document.querySelector('body').style.backgroundColor = '#60b347';
+            document.querySelector('.number').style.width = '30rem';
+        }
+        // greater than secret
+        else if (+guessedNumber > secretNumber) {
+            score--;
+            scoreHTML.textContent = String(score);
+            messageHTML.textContent = `Too high! (~_~)`;
+        }
+        // lower than secretNumber
+        else if (+guessedNumber < secretNumber) {
+            score--;
+            scoreHTML.textContent = String(score);
+            messageHTML.textContent = `Too low! (~_~)`;
+        }
+    }
+    // losing
+    else {
+        scoreHTML.textContent = '0';
+        messageHTML.textContent = `You lost (т_т)`;
+    }
 }
+
+checkButton.onclick = checkGuessedNumber;
+
+/*
+TODO:
+    - Changing the range ?
+    - Winning - blocking everything except again button
+    - Split up into functions
+    - Score update
+    - Again button
+ */
+
+
 
