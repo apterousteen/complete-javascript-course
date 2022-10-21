28c26be72160bda82ca1ae3e79d1396c90074b7f
apterousteen <ayzhan312@live.ru>
2022-10-22 00:56:03 +0500

    08_JS_Behind_The_Scenes_Complete

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// inheritance
+const parent = {
+    year: 1960,
+    calcAge: function () {
+        return 2022 - this.year;
+    },
+};
+
+const child = {
+    year: 2011,
+    __proto__: parent,
+};
+
+console.log(child.calcAge())
+
+// copying object
+// without methods
+const parentClone = JSON.parse(JSON.stringify(parent));
+parentClone.year = 1970;
+
+// with methods, not deep
+const parentClone2 = Object.assign({}, parent);
+parentClone2.year = 1980;
+console.log(parent, parentClone, parentClone2);
